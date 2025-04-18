be39ecb93f8af895f61cb5985f2b3519b4bd9848
Mateusz Samek <samekmat@gmail.com>
2025-04-18 16:05:20 +0200

    chore: setup tailwind

--- contact_management/settings.py
+++ contact_management/settings.py
@@ -44,8 +44,19 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "contacts.apps.ContactsConfig",
+    # third party apps
+    "tailwind",
+    "theme",
+    "contacts",
 ]
+
+TAILWIND_APP_NAME = "theme"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
