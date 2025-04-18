d3bdb251a16458baa9e5cecc0d088dffe531e72c
Mateusz Samek <samekmat@gmail.com>
2025-04-18 19:46:38 +0200

    CONT-003: feature: Add sort and pagination
    Achievements:
    - sort by last_name or data_creation asc and desc
    - paginated by 10 records
    - add docstrings to the view
    - add fixture to bulk load contacts

--- contacts/views.py
+++ contacts/views.py
@@ -12,8 +12,37 @@ from contacts.models import Contact
 
 
 class ContactListView(ListView):
+    """Displays a paginated list of Contact objects with optional sorting."""
+
     model = Contact
     template_name = "contacts/contact_list.html"
+    context_object_name = "contacts"
+    paginate_by = 10
+
+    def get_ordering(self: "ContactListView") -> str:
+        """
+        Determine the ordering of the queryset based on the request GET parameters.
+
+        :return:
+            str: A string indicating the field to order by defaults to "last_name".
+        """
+        ordering = self.request.GET.get("sort", "last_name")
+        if ordering not in ["last_name", "-last_name", "created_at", "-created_at"]:
+            ordering = "last_name"
+        return ordering
+
+    def get_context_data(self: "ContactListView", **kwargs: dict) -> dict:
+        """
+        Add additional context data to the template.
+
+        :param kwargs: Keyword arguments passed to the context.
+
+        :return:
+            dict: The context dictionary with the current sort order appended.
+        """
+        context = super().get_context_data(**kwargs)
+        context["current_sort"] = self.request.GET.get("sort", "last_name")
+        return context
 
 
 class ContactDetailView(DetailView):
