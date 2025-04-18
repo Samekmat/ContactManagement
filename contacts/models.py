a01ea3849d912acf1ee804f8d17604e60dc8fc19
Mateusz Samek <samekmat@gmail.com>
2025-04-18 14:18:24 +0200

    CONT-004: Add ContactStatusChoices model
    Model for contact statuses management

--- contacts/models.py
+++ contacts/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+
+class ContactStatusChoices(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self: "ContactStatusChoices") -> str:
+        """:return: name of the contact status"""
+        return self.name
