7b935e8aa237909e0cd1a9eb18ee86fbb2ed6fb2
Mateusz Samek <samekmat@gmail.com>
2025-04-18 14:36:10 +0200

    CONT-003: Add Contact model
    Main app model responsible for storing Contact data

--- contacts/models.py
+++ contacts/models.py
@@ -7,3 +7,17 @@ class ContactStatusChoices(models.Model):
     def __str__(self: "ContactStatusChoices") -> str:
         """:return: name of the contact status"""
         return self.name
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100, unique=True)
+    city = models.CharField(max_length=50)
+    status = models.ForeignKey(ContactStatusChoices, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self: "Contact") -> str:
+        """:return: name combined of the first name and last name of the contact"""
+        return f"{self.first_name} {self.last_name}"
